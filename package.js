bdb7a19d19115c350aea3cea3f6a6cef8b81de68
jirkakrepl <jiri.krepl@gmail.com>
2021-04-28 16:24:18 +0200

    refactor: renamed gwendal:meteor-simple-schema-i18n
    renamed to jirikrepl:meteor-simple-schema-i18n
    - in previous commit I removed all lang files,
      they are now included in /gulp/src/i18n
    
    - add readme about how it

--- package.js
+++ package.js
@@ -1,25 +1,26 @@
+/* globals Package */
 Package.describe({
-  name: 'gwendall:simple-schema-i18n',
-  summary: 'Internationalization for SimpleSchema',
-  version: '0.3.1',
-  git: 'https://github.com/gwendall/meteor-simple-schema-i18n.git'
+    name: 'jirikrepl:simple-schema-i18n',
+    summary: 'Internationalization for SimpleSchema',
+    version: '0.3.1',
+    git: 'https://github.com/gwendall/meteor-simple-schema-i18n.git',
 });
 
-var packages = [
-  'aldeed:simple-schema@1.3.2',
-  'tap:i18n@1.6.1',
-  'templating@1.1.4',
-  'underscore@1.0.4'
+const packages = [
+    'aldeed:simple-schema@1.3.2',
+    'tap:i18n@1.6.1',
+    'templating@1.1.4',
+    'underscore@1.0.4',
 ];
 
-Package.onUse(function(api, where) {
-  api.use(packages);
-  api.imply(packages);
+Package.onUse(function (api, where) {
+    api.use(packages);
+    api.imply(packages);
 
-  api.addFiles([
-    'package-tap.i18n',
-    'shared/lib.js'
-  ]);
+    api.addFiles([
+        'package-tap.i18n',
+        'shared/lib.js',
+    ]);
 
-  api.addAssets(['package-tap.i18n'], ['client', 'server']);
+    api.addAssets(['package-tap.i18n'], ['client', 'server']);
 });
